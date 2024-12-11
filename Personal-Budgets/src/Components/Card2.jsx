e444ecaa1be9590f7f28f06cd4178c541a3776dd
Correa95 <mathewcorrea19@gmail.com>
2024-12-11 13:24:22 -0500

    working budget component

--- Personal-Budgets/src/Components/Card2.jsx
+++ Personal-Budgets/src/Components/Card2.jsx
@@ -6,12 +6,35 @@ function Card2() {
       <div className="budget">
         <div className="top">
           <h1>Budgets</h1>
+          <button className="btn">See Details</button>
+        </div>
+        <div className="container">
+          <div className="chart"></div>
+          {/* /////////////// */}
+          <div className="category">
+            <div className="entertainmentAmount">
+              <h2 className="entertainment">mathew</h2>
+              <h2 className="amount">10000</h2>
+            </div>
+            <div className="amountBill">
+              <h2 className="bill">mario</h2>
+              <h2 className="amount">10000</h2>
+            </div>
+            <div className="dinningOut">
+              <h2 className="dinning">correa</h2>
+              <h2 className="out">10000</h2>
+            </div>
+            <div className="personalCare">
+              <h2 className="personal">correa</h2>
+              <h2 className="care">10000</h2>
+            </div>
+          </div>
         </div>
       </div>
       <div className="recurBills">
         <div className="top">
-          <small className="btn">See All</small>
           <h1>Recurring Bills</h1>
+          <button className="btn">See All</button>
         </div>
         <div className="bills">
           <div className="paidBills">
